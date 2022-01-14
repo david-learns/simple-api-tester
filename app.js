484f9d8d3694a94bed6ac30ce27d33dc7b455e39
davidg <david.dev.train@gmail.com>
2022-01-14 13:13:06 -0500

    refactor: reorganized for clarity and better error handling

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ const inspectOptions = {
     breakLength: 60
 };
 
+const userErrorMessage = `
+ there was a problem interpreting the command line arguments.
+ make certain the following format is followed:
+
+     node app.js method port path payload
+
+ method: \'get\' or \'post\'
+ port: port on which server is running
+ path: path of api endpoint, must provide at least \'/\'
+ payload: required for post, json, include escapes, no spaces:
+ \"{\\"food\\":\\"juevos rancheros\\",\\"cost\\":9.99}\"`
+
 
 function run() {
     
@@ -36,16 +48,7 @@ function run() {
         
     } else {
         
-        console.log(`
-        there was a problem interpreting the command line arguments.
-        make certain the following format is followed:
-        node app.js method port path payload
-        
-        method: \'get\' or \'post\'
-        port: port on which server is running
-        path: path of api endpoint, must provide at least \'/\'
-        payload: required for post, json, include escapes, no spaces:
-        \"{\\"food\\":\\"juevos rancheros\\",\\"cost\\":9.99}\"`);
+        console.log(userErrorMessage);
         
     }
 }
@@ -55,34 +58,31 @@ function sendGet() {
 
     http.get('http://localhost:' + port + path, res => {
 
-        const responseObj = {
-            STATUS: {
-                code: res.statusCode,
-                message: res.statusMessage
-            }
-        };
-        
         res.setEncoding('utf-8');
         let rawData = '';
         res.on('data', chunk => {
             rawData += chunk;
         });
-        
+
         res.on('end', () => {
-            
-            try {
-                
-                if (res.statusCode === 200) {
-                    responseObj.HEADERS = res.headers;
+
+            const responseObj = {
+                STATUS: {
+                    code: res.statusCode,
+                    message: res.statusMessage
+                },
+                HEADERS: res.headers
+            };
+
+            if (res.statusCode === 200) {
+                try {
                     responseObj.BODY = JSON.parse(rawData);
+                } catch (err) {
+                    console.log('unable to parse response body data as json\n', err.message);
                 }
-
-                console.log(`\nRESPONSE: ${util.inspect(responseObj, inspectOptions)}`);
-
-            } catch (err) {
-                console.log(err.message);
             }
 
+            console.log(`\nRESPONSE: ${util.inspect(responseObj, inspectOptions)}`);
         });
 
     }).on('error', err => {
@@ -95,38 +95,44 @@ function sendPost(options, payload) {
     
     const req = http.request(options, res => {
 
-        const responseObj = {
-            STATUS: {
-                code: res.statusCode,
-                message: res.statusMessage
-            }
-        };
+        const requestObj = { OPTIONS: options, PAYLOAD: payload };
         
         try {
-            
-            const requestObj = { OPTIONS: options, PAYLOAD: JSON.parse(payload) };
-            console.log(`\nREQUEST: ${util.inspect(requestObj, inspectOptions)}`);
-            
-            res.setEncoding('utf-8');
-            let body = '';
-            res.on('data', chunk => {
-                body += chunk;
-            });
-            
-            res.on('end', () => {
-                
-                if (Math.trunc(res.statusCode / 100) === 2) {
-                    responseObj.HEADERS = res.headers;
-                    responseObj.BODY = JSON.parse(body);
-                }
-                
-                console.log(`\nRESPONSE: ${util.inspect(responseObj, inspectOptions)}`);
+            requestObj.PAYLOAD = JSON.parse(payload);
+        } catch (err) {
+            console.log(
+                '\nunable to parse cmd arg payload as json, error:\n ' + err.message + 
+                '\n\npayload: required for post, json, include escapes, no spaces:' +
+                '\n\"{\\"food\\":\\"juevos rancheros\\",\\"cost\\":9.99}\"');
+        }
 
-                });
-                
+        console.log(`\nREQUEST: ${util.inspect(requestObj, inspectOptions)}`);
+
+        res.setEncoding('utf-8');
+        let body = '';
+        res.on('data', chunk => {
+            body += chunk;
+        });
+        
+        res.on('end', () => {
+
+            const responseObj = {
+                STATUS: {
+                    code: res.statusCode,
+                    message: res.statusMessage
+                },
+                HEADERS: res.headers
+            };
+            
+            try {
+                responseObj.BODY = JSON.parse(body);
             } catch (err) {
-                console.log(err.message);
+                console.log('\nerror: unable to parse response body data as json\n', err.message);
+                responseObj.BODY = body.slice(0, body.indexOf('<br>') + 4);
             }
+            
+            console.log(`\nRESPONSE: ${util.inspect(responseObj, inspectOptions)}`);
+        });
     });
 
     req.on('error', err => {
@@ -134,7 +140,7 @@ function sendPost(options, payload) {
     });
 
     req.write(payload);
-    req.end();    
+    req.end();
 }
 
 
